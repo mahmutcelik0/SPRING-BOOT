e9715b97f4f5b0f6e41278fd38c4adb0ed100adb
MAHMUT CELIK <mahmut.382@hotmail.com>
2023-03-02 13:08:23 +0300

    "CELIK"

--- 8-HibernateMappings/OneToMany/Unidirectional/onetomany_unidirectional/src/main/java/com/mahmutcelik/onetomany_unidirectional/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mahmutcelik.onetomany_unidirectional.repository;
-
-import com.mahmutcelik.onetomany_unidirectional.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment,Long> {
-}
--- 8-HibernateMappings/OneToMany/Unidirectional/onetomany_unidirectional/src/main/java/com/mahmutcelik/onetomany_unidirectional/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mahmutcelik.onetomany_unidirectional.repository;
-
-import com.mahmutcelik.onetomany_unidirectional.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post,Long> {
-}
